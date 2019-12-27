7aa55c9a987ba72e4a52123ad5e67f743612ddb4
Michael Hodges <michaelcshodges@gmail.com>
2019-12-27 22:39:04 +1300

    add initial c# practice setup

---
+++ Practice.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace csharp_practice {
+    public class Practice
+    {
+        public string ReturnMessage() {
+            return "Hello C#!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace csharp_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practice practice = new Practice();
+            Console.WriteLine($"Hello World! {practice.ReturnMessage()}");
+        }
+    }
+}
